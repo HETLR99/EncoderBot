5f61be087f16743f5474951e76a97d9a10d2b7c1
HETLR99 <x.x.f.f.f.0@gmail.com>
2024-09-10 00:38:06 +0300

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -5,20 +5,7 @@ if os.path.exists('error.log'):
     os.remove('error.log')
         
 #<-----------LOGGING------------>
-logging.basicConfig(level=logging.INFO, filename='error.log')
-LOG = logging.getLogger("Bot by @soheru")
-LOG.setLevel(level=logging.INFO)
-#<-----------Variables-------------->
-LOG.info('❤️ Checking Bot Variables.....')
-TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '7278911043:AAFkhKG2lL7UUbxP1-1EnIGxsnQtgu7w0zM') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 26786144)) #Telgram Api id
-APP_HASH = os.environ.get('APP_HASH', '4b1c9df2c3a604446539d77929f40d90')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 6169288210))
-MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://mrhex86:mrhex86@cluster0.8pxiirj.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1002164397675)
-BOT_NAME = os.environ.get('BOT_NAME', 'الملك🙃🙃')
-#<---------------Connecting-------------->
+--------->
 if BOT_TOKEN is not None:
     try:
         encoder  = Client('AutoEncoder', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
