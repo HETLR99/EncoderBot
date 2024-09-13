0ea83e17ffddb34b1d06029277368a88945da286
HETLR99 <x.x.f.f.f.0@gmail.com>
2024-09-14 01:15:35 +0300

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -15,7 +15,7 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN', '7278911043:AAFkhKG2lL7UUbxP1-1EnIGxsnQt
 API_ID = int(os.environ.get('API_ID', 21254911)) #Telgram Api id
 APP_HASH = os.environ.get('APP_HASH', '95e927b3c48ac0af4ebb1c3ffeb0069b')# Telgram App hash  
 OWNER_ID = int(os.environ.get('OWNER_ID', 6169288210)
-MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://JXXS7:JXXS7@cluster0.xxkw0.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
+MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://JXXS7:JXXS7@cluster0.xxkw0.mongodb.net/') #MONGO DB FOR ANIME DATA
 FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1002205938557)
 BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
 #<---------------Connecting-------------->
